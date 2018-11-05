5e4e442b9e8a5bde91e93d2ca05d3a57455e9876
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-11-06 00:12:19 +0200

    feat: add aniamtions

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,52 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
-      .then(swReg => {
-        // eslint-disable-next-line
-        console.log('Service Worker is registered', swReg);
-      })
-      .catch(err => {
-        // eslint-disable-next-line
-        console.error('Service Worker Error', err);
+let observer;
+(function () {
+  initServiceWorker();
+  initObserver();
+})();
+
+
+function initObserver () {
+  document.addEventListener('DOMContentLoaded', function () {
+
+    observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          startAnimating(entry.target);
+        }
       });
+    });
+    Array.from(document.querySelectorAll('.animation')).forEach(el => {
+      observer.observe(el);
+    });
+
   });
+}
+
+function startAnimating (el) {
+  const name = el.dataset.animation;
+  el.classList.add(name);
+
+  const onEnd = () => {
+    observer.unobserve(el);
+    el.classList.remove(name, 'animation');
+    el.removeAttribute('data-animation');
+    el.removeEventListener('animationend', onEnd);
+  };
+
+  el.addEventListener('animationend', onEnd);
+}
+
+function initServiceWorker () {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js')
+        .then(swReg => {
+          return swReg;
+        })
+        .catch(err => {
+          // eslint-disable-next-line
+          console.error('Service Worker Error', err);
+        });
+    });
+  }
 }
