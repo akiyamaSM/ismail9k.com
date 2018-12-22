f9c356c8f3530ed9a060dadd186506a8ca5cb8f2
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-12-22 11:01:11 +0300

    feat: update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,15 +5,20 @@ module.exports = [
     description: 'Blexar is An HTML, CSS and JavaScript framework for developing responsive modern web interfaces, focused on usability and minimal sizes, with all the necessary extensions.',
     link: 'http://blexar.s3-website.eu-central-1.amazonaws.com/'
   }, {
-    title: 'Vuse',
+    title: 'Hooper',
     subtitle: 'Vuejs plugin component',
-    description: 'Vuse provides the necessary tools to create an interactive page builder powered with Vuejs framework.',
-    link: 'https://github.com/baianat/vuse'
+    description: 'Vue.js carousal component, optimized to work with Vue.',
+    link: 'https://baianat.github.io/hooper/'
   }, {
     title: 'Verte',
     subtitle: 'Vuejs plugin component',
     description: 'A Color picker component. Built from the bottom to work with Vue.js and also comes in a vanilla version.',
     link: 'https://baianat.github.io/verte/'
+  }, {
+    title: 'Vuse',
+    subtitle: 'Vuejs plugin component',
+    description: 'Vuse provides the necessary tools to create an interactive page builder powered with Vuejs framework.',
+    link: 'https://github.com/baianat/vuse'
   }, {
     title: 'Align',
     subtitle: 'Advanced text-editor',
