dabcad6685aaf184066b315ad402bbced4e51857
abdelrahman <abdelrahman3d@gmail.com>
2019-10-05 14:16:08 +0300

    feat: update content

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,27 +2,36 @@ module.exports = [
   {
     title: 'Blexar',
     subtitle: 'Framework',
-    description: 'Blexar is An HTML, CSS and JavaScript framework for developing responsive modern web interfaces, focused on usability and minimal sizes, with all the necessary extensions.',
-    link: 'http://blexar.s3-website.eu-central-1.amazonaws.com/'
-  }, {
+    description:
+      'An open-source CSS framework based on Flexbox. Blexar built to give maximin usability in small places, with a stunning designed component.',
+    link: 'http://blexar.s3-website.eu-central-1.amazonaws.com/',
+  },
+  {
     title: 'Hooper',
     subtitle: 'Vuejs plugin component',
-    description: 'Vue.js carousal component, optimized to work with Vue.',
-    link: 'https://baianat.github.io/hooper/'
-  }, {
+    description:
+      'A customizable high-performance accessible carousel slider component, optimized for Vue.',
+    link: 'https://baianat.github.io/hooper/',
+  },
+  {
     title: 'Verte',
     subtitle: 'Vuejs plugin component',
-    description: 'A Color picker component. Built from the bottom to work with Vue.js and also comes in a vanilla version.',
-    link: 'https://baianat.github.io/verte/'
-  }, {
+    description:
+      'A Color picker component. Built from the bottom to work with Vue.js and also comes in a vanilla JavaScript version.',
+    link: 'https://baianat.github.io/verte/',
+  },
+  {
     title: 'Vuse',
     subtitle: 'Vuejs plugin component',
-    description: 'Vuse provides the necessary tools to create an interactive page builder powered with Vuejs framework.',
-    link: 'https://github.com/baianat/vuse'
-  }, {
+    description:
+      'Vuse provides the necessary tools to easily create an interactive page builder powered with Vuejs framework.',
+    link: 'https://github.com/baianat/vuse',
+  },
+  {
     title: 'Align',
     subtitle: 'Advanced text-editor',
-    description: 'Align is an advanced text editor that offers a rich post building experience. It has many several components that are customizable to the core.',
-    link: 'https://github.com/baianat/align'
-  }
+    description:
+      'Align offers a unique and rich post building experience, built using vanilla JavaScript.',
+    link: 'https://github.com/baianat/align',
+  },
 ];
--- src/data/socials.js
+++ src/data/socials.js
@@ -1,27 +1,30 @@
 module.exports = [
   {
-    name: 'facebook',
-    href: '//facebook.com/Abdelrahman3D'
-  }, {
     name: 'twitter',
-    href: '//twitter.com/Abdelrahman3D'
-  }, {
+    href: 'https://twitter.com/Abdelrahman3D',
+  },
+  {
     name: 'mail',
-    href: 'mailto:abdelrahman3d@gmail.com?Subject=Hello'
-  }, {
+    href: 'mailto:abdelrahman3d@gmail.com?Subject=Hello',
+  },
+  {
     name: 'github',
-    href: '//github.com/Abdelrahman3D'
-  }, {
+    href: 'https://github.com/Abdelrahman3D',
+  },
+  {
     name: 'medium',
-    href: '//medium.com/@Abdelrahman3D'
-  }, {
+    href: 'https://medium.com/@Abdelrahman3D',
+  },
+  {
     name: 'linkedin',
-    href: '//linkedin.com/in/abdelrahman3d'
-  }, {
+    href: 'https://linkedin.com/in/abdelrahman3d',
+  },
+  {
     name: 'behance',
-    href: '//behance.net/Abdelrahman3D'
-  }, {
+    href: 'https://behance.net/Abdelrahman3D',
+  },
+  {
     name: 'dev',
-    href: '//dev.to/abdelrahman3d'
-  }
+    href: 'https://dev.to/abdelrahman3d',
+  },
 ];
