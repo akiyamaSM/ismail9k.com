6e0b30e6acde2a76fb8cc2719d87cabfd812ce42
abdelrahman <abdelrahman3d@gmail.com>
2019-10-13 13:31:48 +0300

    feat: remove google analytics from layout

--- src/js/script.js
+++ src/js/script.js
@@ -47,16 +47,7 @@ function startAnimating (el) {
   el.addEventListener('animationend', onEnd);
 }
 
-function googleAnalytics () {
-  window.dataLayer = window.dataLayer || [];
-  function gtag () {
-    window.dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-  gtag('config', 'UA-130812708-1');
-}
-
-function initServiceWorker () {
+function initServiceWorker() {
   if (!navigator.serviceWorker) return;
 
   window.addEventListener('load', () => {
