accd994d748ad9f515a06e16e1243b1670c00871
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-12-18 22:59:42 +0300

    feat: update writings

--- src/data/writings.js
+++ src/data/writings.js
@@ -1,15 +1,32 @@
 module.exports = [
   {
+    title: 'Use SVG icons with Vue.js',
+    description:
+      'Many developers prefer to use the font-icons over the SVG ones because it has a simpler syntax, and it integrates seamlessly with almost any front-end component library. In this article, I will try to demonstrate the technics I follow to make it easily to SVG icons.',
+    link: 'https://dev.to/ismail9k/use-svg-icons-with-vue-js-2km7',
+  },
+  {
+    title: 'Use JavaScript Optional Chaining, Today!',
+    description:
+      'Optional Chaining is a new JavaScript API that will make developers\' lives easier :D. Optional Chaining is currently at Stage 3, and soon enough will be part of the language itself, but we can use it, today.',
+    link: 'https://dev.to/ismail9k/use-javascript-optional-chaining-today-2ech',
+  },
+  {
     title: 'Quick CSS Quiz Series',
-    description: 'CSS might be a total mystery to you or a topic that’s near and dear to your heart. Whether you think of CSS as a "necessary evil" or think of it as the power to control the world of technology, CSS is a must-have for anyone working on web applications.',
-    link: 'https://dev.to/abdelrahman3d/quick-css-quiz-1-47i4'
-  }, {
+    description:
+      'CSS might be a total mystery to you or a topic that’s near and dear to your heart. Whether you think of CSS as a "necessary evil" or think of it as the power to control the world of technology, CSS is a must-have for anyone working on web applications.',
+    link: 'https://dev.to/ismail9k/quick-css-quiz-1-47i4',
+  },
+  {
     title: 'CSS Abuse',
-    description: 'It’s all about CSS. All authors here either forgot to use JavaScript or they had disabled it in their browser and can not enable it back again; so they had to finish the job using only pure CSS.',
-    link: 'https://dev.to/abdelrahman3d/css-abuse-31gp'
-  }, {
+    description:
+      'It’s all about CSS. All authors here either forgot to use JavaScript or they had disabled it in their browser and can not enable it back again; so they had to finish the job using only pure CSS.',
+    link: 'https://dev.to/ismail9k/css-abuse-31gp',
+  },
+  {
     title: 'CSS percentage unit, the evil parts',
-    description: 'It should be one of the easiest CSS concepts to understand, the percentage CSS data type represents a percentage value, a percentage is a ratio expressed as a fraction of 100, so it has to do some calculations to get the final computed(absolute) value.',
-    link: 'https://dev.to/abdelrahman3d/css-percentage-unit-the-evil-parts-1g29'
-  }
+    description:
+      'It should be one of the easiest CSS concepts to understand, the percentage CSS data type represents a percentage value, a percentage is a ratio expressed as a fraction of 100, so it has to do some calculations to get the final computed(absolute) value.',
+    link: 'https://dev.to/ismail9k/css-percentage-unit-the-evil-parts-1g29',
+  },
 ];
