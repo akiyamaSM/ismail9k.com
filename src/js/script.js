c3f434ece75a590401b975d784212e986a715a25
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-12-22 20:29:13 +0300

    feat: add dark mode

--- src/js/script.js
+++ src/js/script.js
@@ -3,30 +3,31 @@ let observer;
 
 (function() {
   initServiceWorker();
-  initObserver();
+  document.addEventListener('DOMContentLoaded', function() {
+    initObserver();
+    initThemeSwitcher();
+  });
 })();
 
 function initObserver() {
-  document.addEventListener('DOMContentLoaded', function() {
-    observer = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            startAnimating(entry.target);
-          }
-        });
-      },
-      {
-        threshold: 0.2,
-      }
-    );
+  observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          startAnimating(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+  );
 
-    Array.from(document.querySelectorAll('.animation')).forEach(el => {
-      observer.observe(el);
-    });
-    Array.from(document.querySelectorAll('.fx')).forEach(el => {
-      observer.observe(el);
-    });
+  Array.from(document.querySelectorAll('.animation')).forEach(el => {
+    observer.observe(el);
+  });
+  Array.from(document.querySelectorAll('.fx')).forEach(el => {
+    observer.observe(el);
   });
 }
 
@@ -62,4 +63,25 @@ function initServiceWorker() {
         console.error('Service Worker Error', err);
       });
   });
+}
+
+function initThemeSwitcher() {
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
+  const themeSwitcher = document.querySelector('#themeSwitcher');
+
+  themeSwitcher.checked = mql.matches;
+  themeSwitcher.addEventListener('change', themeChangeHandler);
+  mql.addListener(themeChangeHandler);
+}
+
+
+function themeChangeHandler(e) {
+  console.log('e', e);
+  const body = document.querySelector('body');
+  const themeSwitcher = document.querySelector('#themeSwitcher');
+  const isDark = (e.target && e.target.checked) || e.matches;
+
+  themeSwitcher.checked = isDark;
+  body.classList.remove('theme-dark', 'theme-light');
+  body.classList.add(`theme-${isDark ? 'dark' : 'light'}`);
 }
